4f20dce54e4938cfbb62b7a74286bfa77f617ac4
George <george@gekdesign.com.au>
2024-01-14 20:51:10 +1100

    Remove youtube video

--- src/components/sections/Video.jsx
+++ src/components/sections/Video.jsx
@@ -4,14 +4,14 @@ import Data from "@data/sections/video.json";
 import ModalVideo from "react-modal-video";
 
 const VideoSection = () => {
-  const [isOpen, setOpen] = useState(false);
+  // const [isOpen, setOpen] = useState(false);
 
   return (
     <>
       <div
         data-fancybox
         data-no-swup
-        onClick={() => setOpen(true)}
+        // onClick={() => setOpen(true)}
         className="mil-video-section mil-word-1-trigger mil-accent-trigger"
         style={{ cursor: "pointer" }}
       >
@@ -34,14 +34,14 @@ const VideoSection = () => {
 
         <div className="mil-inner-text mil-text-center mil-p-120-120">
           <div className="container mil-relative">
-            <div className="mil-button mil-button-lg mil-button-rounded mil-button-linear mil-button-light mil-icon mil-appearance mil-mb-30">
+            {/* <div className="mil-button mil-button-lg mil-button-rounded mil-button-linear mil-button-light mil-icon mil-appearance mil-mb-30">
               <span>
                 <i
                   className="fas fa-play"
                   style={{ transform: "translateX(2px)" }}
                 ></i>
               </span>
-            </div>
+            </div> */}
             <h3
               className="mil-light mil-appearance mil-mb-30"
               dangerouslySetInnerHTML={{ __html: Data.title }}
@@ -53,12 +53,12 @@ const VideoSection = () => {
         </div>
       </div>
 
-      <ModalVideo
+      {/* <ModalVideo
         channel="youtube"
         isOpen={isOpen}
         videoId={Data.youtube_video_id}
         onClose={() => setOpen(false)}
-      />
+      /> */}
     </>
   );
 };
