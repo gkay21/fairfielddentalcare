44fd9762e9d878666a65b9274274d0719be11a86
George <george@gekdesign.com.au>
2024-01-14 23:56:49 +1100

    text fix logo for iphone

--- src/layouts/headers/LayoutDefault.js
+++ src/layouts/headers/LayoutDefault.js
@@ -61,7 +61,7 @@ const DefaultHeader = ({ extraClass }) => {
             <div className="mil-mr-15-adapt">
               <Link
                 href="https://www.centaurportal.com/d4w/org-1171/extended_search?randomNumber=21d8305c3ab5a1908e2cf0d54ebc8e33f8259c8b7c5ba501e345e8dce3e10a10&shortVer=true&sourceID="
-                className="mil-button mil-button-md mil-button-dark mil-scale-down-trigger mil-accent-trigger"
+                className="book-button mil-button mil-button-md mil-button-dark mil-scale-down-trigger mil-accent-trigger"
                 target="_blank"
               >
                 Book Now
